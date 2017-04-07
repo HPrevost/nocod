a7bb7546a7c67e70f75069d1cb84369712297289
Hervé PREVOST <prevost.herve@gmail.com>
2017-04-07 11:01:07 +0200

    Maj app

--- app.js
+++ app.js
@@ -1,76 +1,66 @@
-//Modules =======================================
+// Modules =======================================
+
 var compression = require('compression');
 var express = require('express');
 var mysql = require('mysql');
 var sync = require('synchronize');
 
 
+// GLobales =================================
 
-//Application ==================================
+//Chaînes sql, tableau resultats et trouve (true si présence d'enregistrements)
+var sql = '';
+var rows = [];
+var trouve = false;
+
+
+// Application ==================================
+
 var app = express();
 
 
+// Serveur web ===================================
 
-//Base de données ==============================
-
-//Variables globales sql
- 
-//Cahaînes sql
-var sql = '';
-var sql1 = '';
-var sql2 = '';
-
-//Recordsets
-var rst1 = [];
-var rst2 = [];
-var rst3 = [];
-
-
-var trouve = false;
-
-//Connexion ------
-var cn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'JUG@ende4444',
-  database : 'EDITO'
+app.listen(3000, function () {
+  console.log('Server running on port 3000');
 });
 
-cn.connect();
+
+// Base de données ==============================
+
+// Connexion ------
+var cn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'JUG@ende4444',
+  database: 'EDITO'
+});
 
 
-//Requêtes  ------
-var sql = "select * from CLIENT limit 20";
-/*sync.fiber(
-  rst1 = OPN(sql),
-  console.log(rst1)
-);*/
+// Requêtes synchrones ------
 
-console.log('one');
+sync.fiber(function () {
 
- sync.fiber(function() {
- 
+  cn.connect();
+
   var sql = "select * from CLIENT limit 20";
-  var rst1 = OPN(sql);
+  rows = OPN(sql);
+
   if (trouve) {
-    console.log(rst1[0].NomCli);
+
+    console.log(rows[0].NomCli);
+
     sql = "select * from PRODUIT limit 20";
-    rst1 = OPN(sql);
+    rows = OPN(sql);
+
     if (trouve) {
-      console.log(rst1[0].NomProd);
+      console.log(rows[0].NomProd);
     }
+
   }
+
   cn.end();
- });
-console.log('three');
-//Fin Connexion ------
 
-
-
-
-//Serveur web ============================
-app.listen(3000,function() {
-  console.log('Server running on port 3000');
 });
 
 
@@ -81,19 +71,15 @@ function OPN(s) {
   /*
     s = chaîne de requête
   */
- 
+  var data = sync.await(cn.query(s, sync.defer()));
 
-    var data = sync.await(cn.query(s, sync.defer()));
+  if (data.length > 0) {
+    trouve = true;
+  } else {
+    trouve = false;
+  }
 
-    if (data.length>0) {
-      trouve = true;
-    } else {
-      trouve = false;
-    }
-
-    return data;
-
-  
+  return data;
 }
 
 //Ecriture Data
